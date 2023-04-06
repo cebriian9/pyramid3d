2b121a5dc74c8994bb0f03b652bedf4baf82e49c
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2023-04-06 12:47:38 +0200

    intento de menu

--- resources/views/layout/formato.blade.php
+++ resources/views/layout/formato.blade.php
@@ -8,7 +8,6 @@
     <title>@yield('tittle')</title>
     @vite('resources/css/app.css')
     <link rel="shortcut icon" href="{{URL::asset('imagenes/logo.png')}}" type="image/x-icon">
-    <script src="https://kit.fontawesome.com/349b369734.js" crossorigin="anonymous"></script>
 
 </head>
 
@@ -24,14 +23,14 @@
     <!--menu-->
     <nav class="bg-primario text-claro w-full top-0 border">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" class="flex items-center">
+            <a href="{{route('index')}}" class="flex items-center">
                 <img src="{{URL::asset('imagenes/logo.png')}}" class="w-7 sm:w-14 mr-3" alt="Logo">
                 <span class="self-center text-2xl font-semibold ">Pyramid3D</span>
             </a>
             <div class="flex md:order-2">
                 @auth
-                <span class="flex items-center justify-center">
-                    <i class="fa-light fa-user text-2xl"></i> <br>
+                <span class="flex items-center justify-center gap-2">
+                    <i class="fa-solid fa-user"></i> 
                     {{Auth::user()->usuario}}
                 </span>
                 @else
@@ -72,18 +71,7 @@
                             class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                             aria-current="page">Home</a>
                     </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-                    </li>
+                    
                 </ul>
             </div>
         </div>
