b4cca69412413dd8f0e7df3fe38df3fc6d339185
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2023-04-17 11:40:35 +0200

    guardado de direcciones y nombre

--- resources/views/admin/pedidos.blade.php
+++ resources/views/admin/pedidos.blade.php
@@ -3,154 +3,152 @@
 @section('tittle', 'AdminPedidos')
 
 @section('contenido')
-<style>
-    .boton-giratorio {
-        transition: transform 0.5s ease-in-out;
-    }
+    <style>
+        .boton-giratorio {
+            transition: transform 0.5s ease-in-out;
+        }
 
-    .boton-giratorio.girando {
-        transform: rotate(360deg);
-    }
-</style>
+        .boton-giratorio.girando {
+            transform: rotate(360deg);
+        }
+    </style>
 
-<div class="flex justify-center items-center my-4">
-    <span class="text-xl font-semibold">Pedidos</span>
-</div>
+    <div class="flex justify-center items-center my-4">
+        <span class="text-xl font-semibold">Pedidos</span>
+    </div>
 
-<div class=" overflow-x-auto sm:rounded-lg mt-7 mb-4">
-    <table class="w-full text-sm text-left">
-        <thead class=" uppercase bg-primario text-claro">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    ID
-                </th>
-                <th scope="col" class=" px-6 py-3 ">
-                    Usuario
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Material
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Relleno
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Calidad
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Tamaño
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Archivo
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Descargar
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Hecho
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Pedido
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Actualización <button id="mi-boton" class="boton-giratorio ml-3"><i
-                            class="fa-solid fa-rotate-right"></i></button>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($pedidos as $pedido)
-            <tr class="bg-white border-b  hover:bg-gray-400">
-                <td  class="px-6 py-4  ">
-                    {{ $pedido->id }}
-                </td>
-                <td class="px-6 py-4">
-                    {{ $pedido->usuario }}
+    <div class=" overflow-x-auto sm:rounded-lg mt-7 mb-4">
+        <table class="w-full text-sm text-left">
+            <thead class=" uppercase bg-primario text-claro">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        ID
+                    </th>
+                    <th scope="col" class=" px-6 py-3 ">
+                        Usuario
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Material
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Relleno
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Calidad
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Tamaño
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Archivo
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Descargar
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Hecho
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Pedido
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Actualización <button id="mi-boton" class="boton-giratorio ml-3"><i
+                                class="fa-solid fa-rotate-right"></i></button>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($pedidos as $pedido)
+                    <tr class="bg-white border-b  hover:bg-gray-400">
+                        <td class="px-6 py-4  ">
+                            {{ $pedido->id }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $pedido->usuario }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $pedido->material }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $pedido->relleno }}%
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $pedido->calidad }}mm
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $pedido->tamano }}mm
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $pedido->nombreArchivo }}
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="{{ Storage::url($pedido->pathArchivo) }}"
+                                class="font-medium text-blue-500 hover:underline">Descargar archivo</a>
+                        </td>
+                        <td class="px-6 py-4">
+                            @csrf
+                            <label class="relative inline-flex items-center cursor-pointer">
+                                <input type="checkbox" value="{{ $pedido->id }}" name="id"
+                                    class="sr-only peer check-pedido" onclick='updateHecho("{{ $pedido->id }}")'
+                                    @if ($pedido->hecho) checked @endif>
+                                <div
+                                    class="shadow-gray-900 shadow-sm w-11 h-6 bg-gray-400 peer-focus:outline-none rounded-full peer  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-coral-50  after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-secundario-50">
+                                </div>
 
-                </td>
-                <td class="px-6 py-4">
-                    {{ $pedido->material }}
-                </td>
-                <td class="px-6 py-4">
-                    {{ $pedido->relleno }}%
-                </td>
-                <td class="px-6 py-4">
-                    {{ $pedido->calidad }}mm
-                </td>
-                <td class="px-6 py-4">
-                    {{ $pedido->tamano }}mm
-                </td>
-                <td class="px-6 py-4">
-                    {{ $pedido->nombreArchivo }}
-                </td>
-                <td class="px-6 py-4">
-                    <a href="{{ Storage::url($pedido->pathArchivo) }}"
-                        class="font-medium text-blue-500 hover:underline">Descargar archivo</a>
-                </td>
-                <td class="px-6 py-4">
-                    @csrf
-                    <label class="relative inline-flex items-center cursor-pointer">
-                        <input type="checkbox" value="{{$pedido->id}}" name="id" class="sr-only peer check-pedido"
-                            onclick='updateHecho("{{$pedido->id}}")' @if ($pedido->hecho) checked
-                        @endif>
-                        <div
-                            class="shadow-gray-900 shadow-sm w-11 h-6 bg-gray-400 peer-focus:outline-none rounded-full peer  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-coral-50  after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-secundario-50">
-                        </div>
+                            </label>
 
-                    </label>
-
-                </td>
-                <td class="px-6 py-4">
-                    {{ $pedido->created_at }}
-                </td>
-                <td class="px-6 py-4">
-                    {{ $pedido->updated_at }}
-                </td>
-            </tr>
-
-            @endforeach
-        </tbody>
-    </table>
-</div>
-<!--token-->
-<meta name="csrf-token" content="{{ csrf_token() }}">
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $pedido->created_at }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $pedido->updated_at }}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <!--token-->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
 
-<div class="flex justify-center mb-6">
-    {{ $pedidos->links('pagination::tailwind') }}
-</div>
+    <div class="flex justify-center mb-6">
+        {{ $pedidos->links('pagination::tailwind') }}
+    </div>
 
-<!--ajax-->
-
-
-<script>
-    function updateHecho(id) {
-
-        const xhr = new XMLHttpRequest();
-        const form = new FormData();
-        const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
-        form.append('id', id);
-        form.append('_token', token);
-        xhr.open('POST', "{{route('updatePedido')}}");
-        xhr.send(form);
-    }
+    <!--ajax-->
 
 
-    //refrecar datos
+    <script>
+        function updateHecho(id) {
 
-    const boton = document.getElementById("mi-boton");
+            const xhr = new XMLHttpRequest();
+            const form = new FormData();
+            const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
-    boton.addEventListener("click", function () {
-        boton.classList.add("girando");
+            form.append('id', id);
+            form.append('_token', token);
+            xhr.open('POST', "{{ route('updatePedido') }}");
+            xhr.send(form);
+        }
 
-        setTimeout(function () {
-            boton.classList.remove("girando");
-        }, 500);
 
-        location.reload();
-    });
-</script>
+        //refrecar datos
+
+        const boton = document.getElementById("mi-boton");
+
+        boton.addEventListener("click", function() {
+            boton.classList.add("girando");
+
+            setTimeout(function() {
+                boton.classList.remove("girando");
+            }, 500);
+
+            location.reload();
+        });
+    </script>
 
 
 @endsection
